81325df6519f8d4527508c4f6d9688300853cd7f
Miriam Loois <miriamloois@gmail.com>
2026-06-09 15:44:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write(
-    "Wat extra tekst als test. alleen lokaal als het goed is"
+    "Wat extra tekst als test. alleen lokaal als het goed is. en nu ook globaal"
 )
 
